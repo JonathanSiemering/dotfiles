8c0b2ec688f1a3f34d15eda0816a05a5e4dc6d1d
Jonathan Siemering <jsiemering@web.de>
2020-06-06 13:51:20 +0200

    Fix copy dotfiles / Fix update on existing files

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-# go to script folder
-cd $0
+# go to folder of script
+cd `dirname $0`
+script_dir=`pwd`
 
 # install packages
 sudo apt update
@@ -11,12 +12,26 @@ sudo apt -y install zsh git curl
 chsh -s $(which zsh)
 
 # install fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install
+fzf_dir=~/.fzf
+if [ -d $fzf_dir ]; then
+    cd $fzf_dir
+    git pull
+    cd $script_dir
+else
+    git clone --depth 1 https://github.com/junegunn/fzf.git $fzf_dir
+fi
+$fzf_dir/install
 
 # install zsh plugins
-mkdir -p ~/.local/zsh/plugins
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.local/zsh/plugins/
+zsh_syntax_highlighting_dir=~/.local/zsh/plugins/zsh-syntax-highlighting
+if [ -d $zsh_syntax_highlighting_dir ]; then
+    cd $zsh_syntax_highlighting_dir
+    git pull
+    cd $script_dir
+else
+    mkdir -p $zsh_syntax_highlighting_dir
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting_dir
+fi
 
 # insert or replace dotfiles
 cp -f .profile ~/
