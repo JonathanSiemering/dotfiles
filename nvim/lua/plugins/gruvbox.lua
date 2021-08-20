576912458c45eb069c4d7920efbeb843cd4e24f5
Jonathan Siemering <jsiemering@web.de>
2021-08-20 20:49:11 +0200

    Fix nvim gruvbox signcolum background color

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -4,7 +4,14 @@ g.gruvbox_contrast_dark = 'hard'
 
 vim.cmd([[
     colorscheme gruvbox
+
     highlight clear SignColumn
+    highlight GruvboxAquaSign guibg=none
+    highlight GruvboxGreenSign guibg=none
+    highlight GruvboxOrangeSign guibg=none
+    highlight GruvboxPurpleSign guibg=none
+    highlight GruvboxRedSign guibg=none
+    highlight GruvboxYellowSign guibg=none
 
     highlight String guifg=#bcd7de
 ]])
