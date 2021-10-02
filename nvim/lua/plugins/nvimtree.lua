905bfbc10d2a9cbaff61886fd2365ee29ce806d4
Jonathan Siemering <jsiemering@web.de>
2021-10-02 23:06:12 +0200

    Fix nvim nvim-tree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -5,3 +5,6 @@ g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 g.nvim_tree_gitignore = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_lsp_diagnostics = 1
+
+local tree = require('nvim-tree')
+tree.setup()
