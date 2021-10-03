ec61d640f200682878492be5e948e39807e22547
Jonathan Siemering <jsiemering@web.de>
2021-10-03 18:53:34 +0200

    Fix nvim nvimtree

--- nvim/lua/plugins/nvimtree.lua
+++ nvim/lua/plugins/nvimtree.lua
@@ -1,10 +1,13 @@
 local g = vim.g
 
-g.nvim_tree_width = 30
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
 g.nvim_tree_gitignore = 1
 g.nvim_tree_git_hl = 1
-g.nvim_tree_lsp_diagnostics = 1
 
 local tree = require('nvim-tree')
-tree.setup()
+tree.setup{
+    lsp_diagnostics = true,
+    view = {
+        width = 30
+    }
+}
