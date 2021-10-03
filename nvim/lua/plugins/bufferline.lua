d657f8942450254e0c401b58a9defe706ffadc52
Jonathan Siemering <jsiemering@web.de>
2021-10-03 18:53:45 +0200

    Fix nvim bufferline

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -6,8 +6,6 @@ bufferline.setup{
         show_buffer_close_icons = false,
         separator_style = 'thick',
         diagnostics = 'nvim_lsp',
-        numbers = 'ordinal',
-        number_style = '',
         diagnostics_indicator = function(count, level, diagnostics_dict, context)
             local s = ""
             for e, n in pairs(diagnostics_dict) do
