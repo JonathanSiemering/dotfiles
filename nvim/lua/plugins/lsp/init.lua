5246d792bac2b251974f042542a2f13d154bb10e
Jonathan Siemering <jsiemering@web.de>
2021-10-03 20:43:29 +0200

    Add nvim lspconfig sign icons

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -1,3 +1,12 @@
+-- Change sign colum icons
+local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+end
+
+-- Load plugins
 require('plugins.lsp.go')
 require('plugins.lsp.csharp')
 require('plugins.lsp.cpp')
