c6f64b2bad1fbb85cf9a33671507ba272a974524
Jonathan Siemering <jsiemering@web.de>
2022-03-13 21:25:18 +0100

    Add colemak nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,5 +3,6 @@ vim.g.mapleader = ' '
 -- Do not source the default filetype.vim
 vim.g.did_load_filetypes = 1
 
+require('colemak')
 require('settings')
 require('plugins')
---
+++ nvim/lua/colemak.lua
@@ -0,0 +1,9 @@
+vim.cmd([[
+  nnoremap n j| vnoremap n j
+  nnoremap e k| vnoremap e k
+  nnoremap i l| vnoremap i l
+
+  nnoremap j n| vnoremap j n
+  nnoremap k e| vnoremap k e
+  nnoremap l i| vnoremap l i
+]])
