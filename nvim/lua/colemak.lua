620bd953d8455dccdf0f1f08667aabe03058f8c3
Jonathan Siemering <jsiemering@web.de>
2022-03-17 21:15:18 +0100

    Add uppercase colemak

--- nvim/lua/colemak.lua
+++ nvim/lua/colemak.lua
@@ -3,7 +3,15 @@ vim.cmd([[
   nnoremap e k| vnoremap e k
   nnoremap i l| vnoremap i l
 
+  nnoremap N J| vnoremap N J
+  nnoremap E K| vnoremap E K
+  nnoremap I L| vnoremap I L
+
   nnoremap j n| vnoremap j n
   nnoremap k e| vnoremap k e
   nnoremap l i| vnoremap l i
+
+  nnoremap J N| vnoremap J N
+  nnoremap K E| vnoremap K E
+  nnoremap L I| vnoremap L I
 ]])
