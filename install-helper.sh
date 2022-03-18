27949e723d302420d36a45002ce2dfd0fc3be865
Jonathan Siemering <jsiemering@web.de>
2022-03-19 00:23:51 +0100

    Update install-helper

--- install-helper.sh
+++ install-helper.sh
@@ -11,31 +11,6 @@ dotfiles/install.sh
 
 
 
-# +++ Patches +++
-
-# go
-#cat dotfiles/patches/go.txt >> ~/.profile
-
-# wls-go
-#cat dotfiles/patches/wsl-go.txt >> ~/.profile
-
-# wsl ssh agent
-#cat dotfiles/patches/wsl-ssh-agent.txt >> ~/.profile
-
-# wsl2 ssh agent
-#cat dotfiles/patches/wsl2-ssh-agent.txt >> ~/.profile
-
-# cdev
-#cat dotfiles/patches/cdev.txt >> ~/.profile
-
-# gpg tty
-#cat dotfiles/patches/gpg-tty.txt >> ~/.profile
-
-# ssh agent
-#cat dotfiles/patches/ssh-agent.txt >> ~/.profile
-
-
-
 # +++ Windows +++
 
 # .ideavimrc for jetbrain vim emulator
@@ -44,18 +19,3 @@ dotfiles/install.sh
 # .XCompose for wincompose
 #cp -f dotfiles/.XCompose ~/c/.XCompose
 
-
- 
-# +++ neovim plugins +++
-
-# wsl 
-#cp -f dotfiles/nvim/plugin/wsl.vim ~/.config/nvim/plugin/wsl.vim
-
-# go 
-#cp -f dotfiles/nvim/plugin/go.vim ~/.config/nvim/plugin/go.vim
-
-# ccls 
-#cp -f dotfiles/nvim/plugin/ccls.vim ~/.config/nvim/plugin/ccls.vim
-
-# csharp 
-#cp -f dotfiles/nvim/plugin/csharp.vim ~/.config/nvim/plugin/csharp.vim
