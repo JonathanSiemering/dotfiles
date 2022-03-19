cf61809007445d008c86f1e739bf68b0f3d14dfa
Jonathan Siemering <jsiemering@web.de>
2022-03-19 11:23:23 +0100

    Fix fish ssh-agent

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -7,8 +7,7 @@ function ssh_agent
             ss -a | grep -q $SSH_AUTH_SOCK
             if test $status -ne 0
                 rm -f $SSH_AUTH_SOCK
-                bash -c "(setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:\"$HOME/c/npiperelay/npiperelay.exe -ei -s //./pipe/openssh -ssh-agent
-\",nofork &) >/dev/null 2>&1"
+                bash -c "(setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:\"$HOME/c/npiperelay/npiperelay.exe -ei -s //./pipe/openssh -ssh-agent\",nofork &) >/dev/null 2>&1"
             end
         case default
             set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
