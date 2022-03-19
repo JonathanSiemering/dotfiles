847e3a1828e7fc8db751349ed27e10ad8d115a70
Jonathan Siemering <jsiemering@web.de>
2022-03-19 15:54:05 +0100

    Fix fish ssh-agent

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -1,12 +1,12 @@
 switch $SSH_AGENT
     case "wsl"
-        set SSH_AUTH_SOCK ~/c/wsl-ssh-agent/ssh-agent.sock
+        set -x SSH_AUTH_SOCK ~/c/wsl-ssh-agent/ssh-agent.sock
     case "wsl2"
         set -x SSH_AUTH_SOCK "$HOME/.ssh/agent.sock"
         if not ss -a | grep -q "$SSH_AUTH_SOCK";
             rm -f "$SSH_AUTH_SOCK"
             setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/c/npiperelay/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork >/dev/null 2>&1 &
         end
-    case default
-        set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
+    case "openssh"
+        set -x SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
