456a067e48b7a77aebb4b5d14ec0505a1d9a1b33
Jonathan Siemering <jsiemering@web.de>
2022-03-19 18:26:08 +0100

    Remove zsh highlight plugin

--- install.sh
+++ install.sh
@@ -40,17 +40,6 @@ else
 fi
 $fzf_dir/install --key-bindings --completion --no-update-rc
 
-# install zsh plugins
-zsh_syntax_highlighting_dir=~/.local/zsh/plugins/zsh-syntax-highlighting
-if [ -d $zsh_syntax_highlighting_dir ]; then
-    cd $zsh_syntax_highlighting_dir
-    git pull
-    cd $script_dir
-else
-    mkdir -p $zsh_syntax_highlighting_dir
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $zsh_syntax_highlighting_dir
-fi
-
 # neovim
 nvim_dir=~/.nvim
 mkdir -p $nvim_dir
