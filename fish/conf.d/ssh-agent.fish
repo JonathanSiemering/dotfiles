f813f60e1b8e50d4b397712811e8d8ec8e7a553b
Jonathan Siemering <jsiemering@web.de>
2022-03-19 13:27:09 +0100

    Fix fish ssh agent

--- fish/conf.d/ssh-agent.fish
+++ fish/conf.d/ssh-agent.fish
@@ -1,15 +1,12 @@
-function ssh_agent
-    switch $SSH_AGENT
-        case "wsl"
-            set SSH_AUTH_SOCK ~/c/wsl-ssh-agent/ssh-agent.sock
-        case "wsl2"
-            set SSH_AUTH_SOCK $HOME/.ssh/agent.sock
-            ss -a | grep -q $SSH_AUTH_SOCK
-            if test $status -ne 0
-                rm -f $SSH_AUTH_SOCK
-                bash -c "(setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:\"$HOME/c/npiperelay/npiperelay.exe -ei -s //./pipe/openssh -ssh-agent\",nofork &) >/dev/null 2>&1"
-            end
-        case default
-            set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
-    end
+switch $SSH_AGENT
+    case "wsl"
+        set SSH_AUTH_SOCK ~/c/wsl-ssh-agent/ssh-agent.sock
+    case "wsl2"
+        set -x SSH_AUTH_SOCK "$HOME/.ssh/agent.sock"
+        if not ss -a | grep -q "$SSH_AUTH_SOCK";
+            rm -f "$SSH_AUTH_SOCK"
+            setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"$HOME/c/npiperelay/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork >/dev/null 2>&1 &
+        end
+    case default
+        set SSH_AUTH_SOCK $XDG_RUNTIME_DIR/ssh-agent.socket
 end
