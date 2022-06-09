998811974656e5532243058de18f8193e0bd1cf1
Jonathan Siemering <jsiemering@web.de>
2022-06-09 15:31:23 +0200

    Fix fish cdev

--- fish/functions/cdev.fish
+++ fish/functions/cdev.fish
@@ -1,3 +1,4 @@
 function cdev --wraps='cd "$(find  -maxdepth 3 -type d | fzf)"' --description 'alias cdev=cd "$(find  -maxdepth 3 -type d | fzf)"'
-  cd "$(find $CDEV_PATH -maxdepth 3 -type d | fzf)" $argv; 
+  set --local cd_path (find $CDEV_PATH -maxdepth 3 -type d | fzf);
+  cd $cd_path;
 end
