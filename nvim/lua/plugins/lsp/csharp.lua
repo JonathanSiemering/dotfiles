b34fb5a6f3c524177d806130ccae715a4575dec1
Jonathan Siemering <git@siemering.eu>
2022-09-23 23:36:51 +0200

    Remove nvim csharp settings

--- nvim/lua/plugins/lsp/csharp.lua
+++ nvim/lua/plugins/lsp/csharp.lua
@@ -3,9 +3,5 @@ local lsp = require('lspconfig')
 lsp.omnisharp.setup{
     cmd = {
         os.getenv('HOME') .. "/.local/share/nvim/mason/packages/omnisharp/omnisharp",
-    },
-    enable_editorconfig_support = true,
-    enable_ms_build_load_projects_on_demand = false,
-    enable_import_completion = true,
-    analyze_open_documents_only = false
+    }
 }
