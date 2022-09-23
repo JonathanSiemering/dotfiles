1e66598bd61be00c09d9faff4a7e165e81e7aae9
Jonathan Siemering <git@siemering.eu>
2022-09-23 23:19:46 +0200

    Change nvim csharp lsp settings

--- nvim/lua/plugins/lsp/csharp.lua
+++ nvim/lua/plugins/lsp/csharp.lua
@@ -3,8 +3,9 @@ local lsp = require('lspconfig')
 lsp.omnisharp.setup{
     cmd = {
         os.getenv('HOME') .. "/.local/share/nvim/mason/packages/omnisharp/omnisharp",
-        "--languageserver",
-        "--hostPID",
-        tostring(vim.fn.getpid())
-    }
+    },
+    enable_editorconfig_support = true,
+    enable_ms_build_load_projects_on_demand = false,
+    enable_import_completion = true,
+    analyze_open_documents_only = false
 }
