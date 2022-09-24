afb178703296c55926324c6125c147d1c507d078
Jonathan Siemering <git@siemering.eu>
2022-09-24 23:57:18 +0200

    Remove nvim diagnostics highlight change

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -20,17 +20,17 @@ vim.cmd([[
     highlight link DapBreakpointRejected GruvboxRed
     highlight link DapStopped GruvboxAqua
     highlight DapStoppedLineHl guibg=#303030
-
-    highlight link DiagnosticVirtualTextError GruvboxRedSign
-    highlight link DiagnosticVirtualTextWarn GruvboxYellowSign
-    highlight link DiagnosticVirtualTextInfo GruvboxBlueSign
-    highlight link DiagnosticVirtualTextHint GruvboxBlueSign
-    highlight link DiagnosticFloatingError GruvboxRedSign
-    highlight link DiagnosticFloatingWarn GruvboxYellowSign
-    highlight link DiagnosticFloatingInfo GruvboxBlueSign
-    highlight link DiagnosticFloatingHint GruvboxBlueSign
-    highlight link DiagnosticSignError GruvboxRedSign
-    highlight link DiagnosticSignWarn GruvboxYellowSign
-    highlight link DiagnosticSignInfo GruvboxBlueSign
-    highlight link DiagnosticSignHint GruvboxBlueSign
 ]])
+
+    -- highlight link DiagnosticVirtualTextError GruvboxRedSign
+    -- highlight link DiagnosticVirtualTextWarn GruvboxYellowSign
+    -- highlight link DiagnosticVirtualTextInfo GruvboxBlueSign
+    -- highlight link DiagnosticVirtualTextHint GruvboxBlueSign
+    -- highlight link DiagnosticFloatingError GruvboxRedSign
+    -- highlight link DiagnosticFloatingWarn GruvboxYellowSign
+    -- highlight link DiagnosticFloatingInfo GruvboxBlueSign
+    -- highlight link DiagnosticFloatingHint GruvboxBlueSign
+    -- highlight link DiagnosticSignError GruvboxRedSign
+    -- highlight link DiagnosticSignWarn GruvboxYellowSign
+    -- highlight link DiagnosticSignInfo GruvboxBlueSign
+    -- highlight link DiagnosticSignHint GruvboxBlueSign
