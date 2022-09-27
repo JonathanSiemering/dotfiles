ca311e01462356d110f6c3ce96c42c101f6f9086
Jonathan Siemering <git@siemering.eu>
2022-09-27 23:05:07 +0200

    Change fish vi mode keybindings

--- fish/functions/vi_key_bindings.fish
+++
@@ -1,9 +0,0 @@
-# Add for colemak
-function vi_key_bindings
-    bind -s --preset -M default n up-line
-    bind -s --preset -M default e down-line
-    bind -s --preset -M default i forward-char
-
-    bind -s --preset -m insert l repaint-mode
-    bind -s --preset -m insert L beginning-of-line repaint-mode
-end
