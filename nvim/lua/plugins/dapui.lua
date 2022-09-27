71cd2f8aa65dd08e88d6fa7d447c3af562393b62
Jonathan Siemering <git@siemering.eu>
2022-09-27 19:36:54 +0200

    Change nvim dap ui layout

--- nvim/lua/plugins/dapui.lua
+++ nvim/lua/plugins/dapui.lua
@@ -1,3 +1,24 @@
 local dapui = require('dapui')
 
-dapui.setup({})
+dapui.setup({
+    layouts = {
+        {
+            position = "right",
+            size = 60,
+            elements = {
+                "stacks",
+                {id = "breakpoints", size = 0.15},
+                {id = "watches", size = 0.15},
+            },
+        },
+        {
+            position = "bottom",
+            size = 0.2,
+            elements = {
+                "repl",
+                {id = "console", size = 0.20},
+            },
+        },
+    },
+})
+
