71bdec9f05600b2828a78e55e39981f6e5ab2ea9
Jonathan Siemering <git@siemering.eu>
2022-10-28 22:49:18 +0200

    Fix fish nvim path

--- fish/conf.d/path.fish
+++ fish/conf.d/path.fish
@@ -1,1 +1,1 @@
-set -gx PATH $PATH $HOME/bin $HOME/.local/bin $HOME/.nvim/nvim-linux64/bin $HOME/go/bin /usr/local/go/bin
+set -gx PATH $PATH $HOME/bin $HOME/.local/bin $HOME/.local/nvim/nvim-linux64/bin $HOME/go/bin /usr/local/go/bin
