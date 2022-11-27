809b34cdd66d6cbc482b460da9bfb7d9515630b8
Jonathan Siemering <git@siemering.eu>
2022-11-27 19:29:59 +0100

    Add install.ps1

---
+++ install.ps1
@@ -0,0 +1,44 @@
+$ScriptDir = pwd
+
+# Ripgrep
+$RipgrepDir = "$Home\.local\ripgrep"
+if (!(Test-Path -Path $RipgrepDir)) {
+    mkdir -p $RipgrepDir
+}
+cd $RipgrepDir
+
+if (Test-Path -Path ripgrep) {
+    Remove-Item ripgrep -Recurse -Force -Confirm:$false
+}
+wget -O ripgrep.zip https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep-13.0.0-x86_64-pc-windows-msvc.zip
+Expand-Archive -DestinationPath ripgrep ripgrep.zip
+rm ripgrep.zip
+cd $ScriptDir
+
+# Nvim
+$NvimDir = "$Home\.local\nvim"
+if (!(Test-Path -Path $NvimDir)) {
+    mkdir -p $NvimDir
+}
+cd $NvimDir
+
+if (Test-Path -Path nvim) {
+    Remove-Item nvim -Recurse -Force -Confirm:$false
+}
+wget -O nvim.zip https://github.com/neovim/neovim/releases/download/stable/nvim-win64.zip
+Expand-Archive nvim.zip
+rm nvim.zip
+cd $ScriptDir
+
+# Nvim config
+$NvimConfigDir = "$Home\AppData\Local\nvim"
+if (!(Test-Path -Path $NvimConfigDir)) {
+    mkdir -p $NvimConfigDir
+}
+cd $NvimConfigDir
+
+Remove-Item * -Recurse -Force -Confirm:$false
+cp -r $ScriptDir\nvim\* .
+
+"" > update
+cd $ScriptDir
