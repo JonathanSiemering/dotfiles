68729f34dba4fa0064078174091d6253d7728649
Jonathan Siemering <git@siemering.eu>
2023-01-05 22:58:54 +0100

    Fix install-helper.sh

--- install-helper.sh
+++ install-helper.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 # specify os
-# export DOTFILES_OS = "ubuntu"
-# export DOTFILES_OS = "arch"
-# export DOTFILES_OS = "macos"
+# export DOTFILES_OS="ubuntu"
+# export DOTFILES_OS="arch"
+# export DOTFILES_OS="macos"
 
 # normal installation
 cd dotfiles
