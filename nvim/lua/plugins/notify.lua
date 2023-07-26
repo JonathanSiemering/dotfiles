b42e5726cf8d4875d5024e923b36bcb2977c52d8
Jonathan Siemering <git@siemering.eu>
2023-07-26 15:45:03 +0200

    Add nvim lsp notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -1,12 +1,29 @@
 return {
-    'rcarriga/nvim-notify',
-    config = function()
-        local notify = require('notify')
-        local telescope = require('telescope')
 
-        notify.setup({})
-        vim.notify = notify
-        telescope.load_extension('notify')
-    end
+    -- Notify
+    {
+        'rcarriga/nvim-notify',
+        config = function()
+            local notify = require('notify')
+            local telescope = require('telescope')
+
+            notify.setup({})
+            vim.notify = notify
+            telescope.load_extension('notify')
+        end
+    },
+
+    -- Lsp notify
+    {
+        'mrded/nvim-lsp-notify',
+        dependencies = {
+            'rcarriga/nvim-notify',
+            'neovim/nvim-lspconfig',
+        },
+        config = function()
+            require('lsp-notify').setup({})
+        end
+    },
+
 }
 
