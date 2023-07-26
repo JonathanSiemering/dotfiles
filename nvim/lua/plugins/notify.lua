4d556a8e001c66c6f7166b18d35d0d2f84211519
Jonathan Siemering <git@siemering.eu>
2023-07-26 19:50:08 +0200

    Remove nvim lsp notify

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -13,17 +13,5 @@ return {
         end
     },
 
-    -- Lsp notify
-    {
-        'mrded/nvim-lsp-notify',
-        dependencies = {
-            'rcarriga/nvim-notify',
-            'neovim/nvim-lspconfig',
-        },
-        config = function()
-            require('lsp-notify').setup({})
-        end
-    },
-
 }
 
