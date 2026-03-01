f159ca8cb65afd8166c99ce02ed8fe7a0424c54e
Jonathan Siemering <git@siemering.eu>
2026-03-01 20:08:08 +0100

    Fix

--- nvim/lua/plugins/github-copilot-chat.lua
+++ nvim/lua/plugins/github-copilot-chat.lua
@@ -12,15 +12,4 @@ return {
         },
         auto_fold = true,
     },
-    config = function()
-        -- Auto-command to customize chat buffer behavior
-        vim.api.nvim_create_autocmd('BufEnter', {
-          pattern = 'copilot-*',
-          callback = function()
-            vim.opt_local.relativenumber = false
-            vim.opt_local.number = false
-            vim.opt_local.conceallevel = 0
-          end,
-        })
-    end,
 }
